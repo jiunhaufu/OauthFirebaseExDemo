84de33f7b2f73c55c7817ef3d25573ded135fd42
傅俊皓 <alfie.fu@toplogis.com>
2018-01-26 19:38:05 +0800

    使用實作embedded facebook登入

--- app/src/main/java/fu/alfie/com/oauthfirebaseexdemo/MainActivity.java
+++ app/src/main/java/fu/alfie/com/oauthfirebaseexdemo/MainActivity.java
@@ -16,6 +16,13 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -30,6 +37,7 @@ import com.google.firebase.FirebaseException;
 import com.google.firebase.FirebaseTooManyRequestsException;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseUser;
@@ -54,6 +62,8 @@ public class MainActivity extends AppCompatActivity {
     public PhoneAuthProvider.ForceResendingToken mResendToken;
     //google
     public GoogleSignInClient mGoogleSignInClient;
+    //facebook
+    public CallbackManager mCallbackManager;
 
 
     @Override
@@ -73,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
 
         initPhoneLogin();
         initGoogleLogin();
+        initFacebookLogin();
     }
 
     @Override
@@ -147,6 +158,7 @@ public class MainActivity extends AppCompatActivity {
                 // ...
             }
         }
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     public void emailLogin(String email, String password) {
@@ -292,6 +304,45 @@ public class MainActivity extends AppCompatActivity {
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, GOOGLE_SIGN_IN);
     }
+
+    private void initFacebookLogin(){
+        mCallbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = findViewById(R.id.button_facebook_login);
+        loginButton.setReadPermissions("email", "public_profile");
+        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                // ...
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                // ...
+            }
+        });
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
 
     //facebook
     public void printhashkey(){
